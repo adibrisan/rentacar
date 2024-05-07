6308861176af39cb5789f31d656869a6bc72d4f8
adibrisan <adi.brisan@yahoo.com>
2024-05-07 17:40:35 +0300

    getCars hook

--- client/src/hooks/useGetCarList.js
+++ client/src/hooks/useGetCarList.js
@@ -3,25 +3,30 @@ import api from "../utils/axiosInstance";
 import toast from "react-hot-toast";
 
 const useGetCarList = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const [carList, setCarList] = useState([]);
 
   useEffect(() => {
     const getCars = async () => {
       try {
+        setIsLoading(true);
         const res = await api.get("/cars");
         if (res.status === 200) {
           setCarList(res.data);
+          setIsLoading(false);
         } else {
+          setIsLoading(false);
           toast.error("Error retrieving cars data.");
         }
       } catch (err) {
+        setIsLoading(false);
         console.log(err);
         toast.error("Error retrieving cars data.");
       }
     };
     getCars();
   }, []);
-  return carList;
+  return { carList, isLoading };
 };
 
 export default useGetCarList;
--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,18 +1,20 @@
-import { Row, Col } from "antd";
+import { Row, Col, Spin } from "antd";
 import CarCard from "../../components/CarCard/CarCard";
 import useGetCarList from "../../hooks/useGetCarList";
 
 const HomePage = () => {
-  const cars = useGetCarList();
+  const { carList: cars, isLoading } = useGetCarList();
   return (
     <main>
-      <Row justify="center" style={{ margin: "25px 25px" }} gutter={20}>
-        {cars.map((car) => (
-          <Col key={car._id} style={{ marginTop: "20px" }}>
-            <CarCard carDetails={car} />
-          </Col>
-        ))}
-      </Row>
+      <Spin spinning={isLoading}>
+        <Row justify="center" style={{ margin: "25px 25px" }} gutter={20}>
+          {cars.map((car) => (
+            <Col key={car._id} style={{ marginTop: "20px" }}>
+              <CarCard carDetails={car} />
+            </Col>
+          ))}
+        </Row>
+      </Spin>
     </main>
   );
 };
