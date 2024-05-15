ff4a453860f1553dc353c1681386916b7d652d43
adibrisan <adi.brisan@yahoo.com>
2024-05-15 22:05:00 +0300

    implemented delete order by id endpoint

--- server/routes/order.js
+++ server/routes/order.js
@@ -1,8 +1,17 @@
 import { Router } from "express";
-import { createOrder, getUserOrders } from "../controllers/order-controller.js";
+import {
+  createOrder,
+  getUserOrders,
+  deleteOrderById,
+} from "../controllers/order-controller.js";
 import { verifyTokenAndAuthorization } from "../middleware/verifyToken.js";
 
 export const orderRouter = Router();
 
 orderRouter.post("/newOrder", verifyTokenAndAuthorization, createOrder);
 orderRouter.get("/orders/:userId", verifyTokenAndAuthorization, getUserOrders);
+orderRouter.delete(
+  "/deleteOrder/:orderId",
+  verifyTokenAndAuthorization,
+  deleteOrderById
+);
