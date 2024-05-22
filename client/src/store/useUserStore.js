9e38f6711764e751d1c3cd17d326d441b0e85e92
adibrisan <adi.brisan@yahoo.com>
2024-05-22 21:57:19 +0300

    use of devtools

--- client/src/store/useUserStore.js
+++ client/src/store/useUserStore.js
@@ -1,10 +1,15 @@
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 
-const useUserStore = create((set) => ({
-  currentUser: null,
-  setCurrentUser: (currentUser) => set({ currentUser }),
-  isLoadingUserData: true,
-  setIsLoadingUserData: (isLoadingUserData) => set({ isLoadingUserData }),
-}));
+const useUserStore = create(
+  devtools((set) => ({
+    currentUser: null,
+    setCurrentUser: (currentUser) =>
+      set({ currentUser }, false, "setCurrentUser"),
+    isLoadingUserData: true,
+    setIsLoadingUserData: (isLoadingUserData) =>
+      set({ isLoadingUserData }, false, "setIsLoadingUserData"),
+  }))
+);
 
 export default useUserStore;
